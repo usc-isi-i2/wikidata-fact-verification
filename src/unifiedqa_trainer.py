8f1d0fd11b514ba38c395989e8b486ca1a2825a4
abhinav-kumar-thakur <abhinavkumarthakur786@gmail.com>
2022-08-03 17:43:09 -0700

    Avoid saving best model at epoch 0 if score is less than pretrained

--- src/unifiedqa_trainer.py
+++ src/unifiedqa_trainer.py
@@ -83,6 +83,7 @@ class UnifiedQATrainer:
         results_df = pd.DataFrame(predictions_to_save, columns=['Input', 'Correct', 'Prediction', 'Is Correct prediction'])
         results_df.to_excel(f'{self.run_files}/predictions_{dataset_name}_{"pretrained" if epoch == -1 else epoch}.xlsx', index=False)
         if epoch == -1 or dataset_name == 'train':
+            self.best_score[dataset_name] = f1
             return
 
         if f1 > self.best_score[dataset_name]:
