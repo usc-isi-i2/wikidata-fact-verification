1752bcb9bf9afd343653a96b3c5d787641171afd
saggu <amandeep.s.saggu@gmail.com>
2022-08-17 12:07:44 -0700

    use data parallelism if ngpus > 1

--- train.py
+++ train.py
@@ -2,7 +2,8 @@ import argparse
 import json
 import os.path
 
-from accelerate import Accelerator
+# from accelerate import Accelerator
+import torch
 from torch.utils.data import ConcatDataset, DataLoader
 from transformers import Adafactor
 from transformers import T5Tokenizer, T5ForConditionalGeneration
@@ -14,7 +15,8 @@ from src.utils import prepare_run_files_directory
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train T5 unifiedQA model')
-    parser.add_argument('--experiment', type=str, default='check', help='Experiment to run from experiments/config/exp_<experiment.json')
+    parser.add_argument('--experiment', type=str, default='check',
+                        help='Experiment to run from experiments/config/exp_<experiment.json')
     args = parser.parse_args()
 
     with open(os.path.join('experiments/configs', f'exp_{args.experiment}.json')) as f:
@@ -27,21 +29,29 @@ if __name__ == '__main__':
 
     logfile = os.path.join(f'experiments/logs/exp_{args.experiment}.tsv')
 
-    accelerator = Accelerator()
-    device = accelerator.device
+    # accelerator = Accelerator()
+    # device = accelerator.device
+    n_gpu = torch.cuda.device_count()
+    device = 'cuda' if n_gpu > 0 else 'cpu'
     print(f'Running on device: {device}')
 
     model_name = f'allenai/unifiedqa-v2-t5-{model_size}-1251000'
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name)
-    model.to(device)
+    if n_gpu > 1:
+        model = torch.nn.DataParallel(model)
+    if n_gpu > 0:
+        model.to(torch.device("cuda"))
+    if n_gpu == 0:
+        model.to("cpu")
 
     train_datasets = {}
     for dataset in configs['train_datasets']:
         dataset_type, dataset_name = dataset.split(':')
         train_datasets[dataset] = get_dataset_functions[dataset_type](dataset_name)
 
-    train_dataloader = DataLoader(ConcatDataset(train_datasets.values()), shuffle=True, batch_size=configs['train_batch_size'])
+    train_dataloader = DataLoader(ConcatDataset(train_datasets.values()), shuffle=True,
+                                  batch_size=configs['train_batch_size'])
 
     eval_datasets = {}
     for dataset in configs['eval_datasets']:
@@ -51,7 +61,8 @@ if __name__ == '__main__':
     optimizer = Adafactor(model.parameters(), scale_parameter=True, relative_step=True, warmup_init=True, lr=None)
     lr_scheduler = AdafactorSchedule(optimizer)
 
-    trainer = UnifiedQATrainer(run_files, model, tokenizer, optimizer, lr_scheduler, device, eval_batch_size=configs['eval_batch_size'])
+    trainer = UnifiedQATrainer(run_files, model, tokenizer, optimizer, lr_scheduler, device,
+                               eval_batch_size=configs['eval_batch_size'])
     config_string = f'Run configs from experiments/configs/exp_{args.experiment}.json: {configs}'
 
     with open(logfile, 'w') as f:
