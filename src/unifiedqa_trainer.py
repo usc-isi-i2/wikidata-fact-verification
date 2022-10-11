8e5306cf03cdc7524d7f5c8a9101271745858af6
saggu <amandeep.s.saggu@gmail.com>
2022-10-11 15:53:36 -0700

    update parsed datae

--- src/unifiedqa_trainer.py
+++ src/unifiedqa_trainer.py
@@ -167,17 +167,26 @@ class UnifiedQATrainer:
 
     @staticmethod
     def parse_date(str_with_date: str, nlp: spacy.lang.en.English, requires):
-        spacy_docs = nlp(str_with_date)
-        spacy_dates = [x for x in spacy_docs.ents if x.label_ == 'DATE']
+        # spacy_docs = nlp(str_with_date)
+        # spacy_dates = [x for x in spacy_docs.ents if x.label_ == 'DATE']
         parsed_dates = []
-        if spacy_dates:
-            for sd in spacy_dates:
-                parsed_date = dateparser.parse(sd.text, settings={'PREFER_DAY_OF_MONTH': 'first',
-                                                                  'REQUIRE_PARTS': requires})
-                if parsed_date:
-                    parsed_dates.append({'date': f'{parsed_date.year}-{parsed_date.month:0>2}-{parsed_date.day:0>2}',
-                                         'year': parsed_date.year,
-                                         'month': f'{parsed_date.month:0>2}',
-                                         'day': f'{parsed_date.day:0>2}',
-                                         'orig_date': sd.text})
+        # if spacy_dates:
+        #     for sd in spacy_dates:
+        #         parsed_date = dateparser.parse(sd.text, settings={'PREFER_DAY_OF_MONTH': 'first',
+        #                                                           'REQUIRE_PARTS': requires})
+        #         if parsed_date:
+        #             parsed_dates.append({'date': f'{parsed_date.year}-{parsed_date.month:0>2}-{parsed_date.day:0>2}',
+        #                                  'year': parsed_date.year,
+        #                                  'month': f'{parsed_date.month:0>2}',
+        #                                  'day': f'{parsed_date.day:0>2}',
+        #                                  'orig_date': sd.text}
+        parsed_date = dateparser.parse(str_with_date, settings={'PREFER_DAY_OF_MONTH': 'first',
+                                                                'REQUIRE_PARTS': requires})
+        if parsed_date:
+            parsed_dates.append(
+                {'date': f'{parsed_date.year}-{parsed_date.month:0>2}-{parsed_date.day:0>2}',
+                 'year': parsed_date.year,
+                 'month': f'{parsed_date.month:0>2}',
+                 'day': f'{parsed_date.day:0>2}',
+                 'orig_date': str_with_date})
         return parsed_dates
